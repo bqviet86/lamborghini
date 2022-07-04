4bc85856fcd66f24bcf8b43fdd60acd3ed0ed9e4
bqviet86 <bqvct4211@gmail.com>
2022-07-04 12:26:36 +0700

    update

---
+++ assets/js/slider2-repl.js
@@ -0,0 +1,15 @@
+// SWIPER SLIDER
+let swiperThumbs = new Swiper('.mySwiper-thumb', {
+    loop: true,
+    spaceBetween: 0,
+    effect: 'fade',
+});
+
+let swiperSlide = new Swiper('.mySwiper-slide', {
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+    thumbs: {
+        swiper: swiperThumbs,
+    },
+});
